cce25f9abc0f595903a51e0e8a733842f4a661fd
陈伟(10024011) <chenw-i@glodon.com>
2019-10-18 11:19:49 +0800

    商品详情页改造

--- pages/shop/goods2/details.js
+++ pages/shop/goods2/details.js
@@ -9,7 +9,35 @@ Page({
             'https://image.weilanwl.com/img/4x3-3.jpg',
             'https://image.weilanwl.com/img/4x3-4.jpg',
         ],
-        modalAttrShow:false
+        modalAttrShow:false,
+        tags:[
+          {
+            id:1,
+            name:'低帮黑色',
+            color:'red',
+            selected:false,
+          }, {
+            id: 2,
+            name: '高帮白色',
+            color: 'grey',
+            selected: false,
+          }, {
+            id: 3,
+            name: '低帮黄色',
+            color: 'red',
+            selected: false,
+          }, {
+            id: 4,
+            name: '低帮黄色',
+            color: 'red',
+            selected: false,
+          }, {
+            id: 5,
+            name: '低帮黄色',
+            color: 'red',
+            selected: false,
+          },
+        ]
     },
     onLoad: function (options) {
         let that = this;
@@ -23,5 +51,8 @@ Page({
     this.setData({
       modalAttrShow: false
     })
+  },
+  selectTag(e){
+    console.log(e.detail.current)
   }
 });
